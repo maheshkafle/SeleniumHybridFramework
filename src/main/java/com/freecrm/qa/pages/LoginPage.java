0d017cc96433d63eba982d628f26bb35f649cd92
kaflemahesh <mahesh.kafle@smarsh.com>
2021-09-12 17:25:03 +0545

    Implemented Login Page Test scenario

--- src/main/java/com/freecrm/qa/pages/LoginPage.java
+++ src/main/java/com/freecrm/qa/pages/LoginPage.java
@@ -9,33 +9,42 @@ import static java.lang.Thread.*;
 
 public class LoginPage extends TestBase {
 
-    // PageFactory - Object Repositories
-    @FindBy(name="email")
+    // PageFactory - Object Repositories - Collection of WebElements - we use @FindBy TestNG annotation
+    @FindBy(name="username")
     WebElement username;
 
     @FindBy(name="password")
     WebElement password;
 
-    @FindBy(css=".fluid")
+    @FindBy(xpath="//input[contains(@value, 'Login')]")
     WebElement loginBtn;
 
     @FindBy(xpath="//button[contains(text(), 'Sign Up']")
     WebElement signUpBtn;
 
-    @FindBy(xpath="//img[contains(@class,'img-responsive')]")
+    @FindBy(xpath = "//img[contains(@class, 'img-responsive')]")
     WebElement crmLogo;
 
-    // Initializing the Page Objects
+    // Initializing the Page Objects with Constructor - Interview Question
     public LoginPage(){
+
         PageFactory.initElements(driver, this);
+
     }
 
     // Actions
     public String validateLoginPageTitle()  {
+
         return driver.getTitle();
     }
 
+    public boolean validateCRMLogo() {
+
+        return crmLogo.isDisplayed();
+    }
+
     public HomePage login(String uname, String pwd){
+
         username.sendKeys(uname);
         password.sendKeys(pwd);
         loginBtn.click();
