f403da8e3a4df6fae54915bb552ea4a99bb6788f
kaflemahesh <mahesh.kafle@smarsh.com>
2021-09-12 17:26:40 +0545

    Updated TestUtil class

--- src/main/java/com/freecrm/qa/utils/TestUtil.java
+++ src/main/java/com/freecrm/qa/utils/TestUtil.java
@@ -1,7 +1,9 @@
 package com.freecrm.qa.utils;
 
 public class TestUtil {
-
+    // Lets say our site is taking long time to load then we need to change value then
+    // in that scenario its better to put this value in one place and it becomes easy to
+    // change them
     public static long PAGE_LOAD_TIMEOUT = 20;
     public static long IMPLICIT_WAIT = 10;
 }
