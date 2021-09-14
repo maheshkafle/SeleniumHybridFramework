5fa9453549c271534f0cdf9a0a1144cc01ba75c0
kaflemahesh <mahesh.kafle@smarsh.com>
2021-09-14 23:56:00 +0545

    Added method to take screenshot if an exception occurs which gets called from WebEventListener class

--- src/main/java/com/freecrm/qa/utils/TestUtil.java
+++ src/main/java/com/freecrm/qa/utils/TestUtil.java
@@ -2,11 +2,15 @@ package com.freecrm.qa.utils;
 
 import com.freecrm.qa.base.TestBase;
 import org.apache.commons.compress.archivers.dump.InvalidFormatException;
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -61,5 +65,13 @@ public class TestUtil extends TestBase {
             }
         }
         return data;
+    }
+
+    // Method to take screenshot if an exception occurs under screenshots folder and gets called from WebEventListener class
+    public static void takeScreenshotAtEndOfTest() throws IOException {
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String currentDir = System.getProperty("user.dir");
+
+        FileUtils.copyFile(srcFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
     }
 }
